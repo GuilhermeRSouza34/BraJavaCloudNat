91e18c33b1730e3c22ca74a3fb734b242385b603
Guilherme <guilhermersouza34@gmail.com>
2024-08-17 11:02:40 -0300

    Projeto desenvolvido para o teste pratico da empresa Braxxy

---
+++ device-monitoring/src/main/java/com/monitoring/device_monitoring/DeviceMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.monitoring.device_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviceMonitoringApplication.class, args);
+	}
+
+}
---
+++ device-monitoring/src/test/java/com/monitoring/device_monitoring/DeviceMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monitoring.device_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
