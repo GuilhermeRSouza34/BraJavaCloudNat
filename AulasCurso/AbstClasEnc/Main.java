7602a49495bf4f32c8af251a41161754039c3abb
Guilherme <guilhermersouza34@gmail.com>
2025-04-17 22:31:18 -0300

    aulas sobre classes abstratas

--- AulasCurso/AbstClasEnc/Main.java
+++ AulasCurso/AbstClasEnc/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         var male = new Person();
-        male.name = "John";
-        male.age = 30;
+        male.setName("João");
+        male.setAge(30);
         var female = new Person();
-        female.name = "Mary";
-        female.age = 25;
+        female.setName("Maria");
+        female.setAge(25);;
         System.out.println("Name: " + male.getName() + " age: " + male.getAge());
         System.out.println("Name: " + female.getName() + " age: " + female.getAge());     
     }   
