420bb74ad87cc9218f2f6b2102b9713a9d2a8347
Guilherme <guilhermersouza34@gmail.com>
2025-04-17 23:00:36 -0300

    aula sobre Records

--- AulasCurso/Records/Main.java
+++ AulasCurso/Records/Main.java
@@ -1,3 +1,10 @@
 public class Main {
-    
+    public static void main(String[] args) {
+        var person = new Person("Guilherme", 22);
+        System.out.println(person);
+        System.out.println(person.name());
+        var person2 = new Person("Souza", 30);
+        System.out.println(person2);
+        System.out.println(person2.name());
+    }
 }
